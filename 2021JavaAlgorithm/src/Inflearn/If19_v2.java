b793c467e4205cf7ee0c3a564dc92078a338bb6d
jieun-dev1 <jkdev1324@gmail.com>
2022-01-31 11:50:54 +0900

    cnt 사용

---
+++ 2021JavaAlgorithm/src/Inflearn/If19_v2.java
@@ -0,0 +1,32 @@
+package Inflearn;
+
+import java.util.Scanner;
+
+public class If19_v2 {
+
+	public int solution(int num, int[] arr) {
+		int[] score = new int[num];
+		int answer = 0;
+		int cnt = 0;
+		for (int i = 0; i < num; i++) {
+			if (arr[i] == 1) {
+				cnt++;
+				answer += cnt;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		If19 T = new If19();
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+		for (int i = 0; i < num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(T.solution(num, arr));
+	}
+
+}
