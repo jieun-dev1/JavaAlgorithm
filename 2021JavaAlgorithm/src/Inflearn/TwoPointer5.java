643d454027f8fe744f64b88a2a616fd499604cb6
jieun-dev1 <jkdev1324@gmail.com>
2022-04-09 16:48:02 +0900

    연속된 자연수의 합

--- 2021JavaAlgorithm/src/Inflearn/TwoPointer5.java
+++ 2021JavaAlgorithm/src/Inflearn/TwoPointer5.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 //시도한 전략 (1) TreeSet 에서 Iterate 하기 - 키를 정렬하고 고유값 (여기서 막힘) -> 만들 필요 없었음.  
 //(2)HashMap 끼리 비교하는 게 중점.. 
 
-public class Anagram {
+public class TwoPointer5 {
 	// A를 기준으로 put을 해주고, b에서 key를 훑으면서, 만약 map의 (a의) key가 b의 key에 안들어있으면.
 	// 혹은 b의 키가 map에는 없으면 return no.
 
@@ -30,7 +30,7 @@ public class Anagram {
 	public static void main(String[] args) {
 		// 메인 함수에서는 입력을 받는다.
 		// static 에서 메소드를 호출하려면, 객체를 생성해야 한다.
-		Anagram T = new Anagram();
+		TwoPointer5 T = new TwoPointer5();
 		Scanner sc = new Scanner(System.in);
 		String str1 = sc.next();
 		String str2 = sc.next();
