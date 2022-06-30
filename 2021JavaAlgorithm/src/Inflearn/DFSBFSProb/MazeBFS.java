f41f524cd667d3f647d16bc7084a670c1518fa8c
jieun-dev1 <jkdev1324@gmail.com>
2022-07-01 00:46:54 +0900

    미로찾기 - bfs 답안 수정

--- 2021JavaAlgorithm/src/Inflearn/DFSBFSProb/MazeBFS.java
+++ 2021JavaAlgorithm/src/Inflearn/DFSBFSProb/MazeBFS.java
@@ -4,6 +4,12 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+/**
+ * DFS 와의 차이점.
+ * DIS[][] 배열을 따로 만들어줘서 길이 값을 표시해준다. q 가 빈다는 것 - 7,7에 도달해서 더 넣을 게 없는 것.
+ * 답이 없을 경우는 -1.
+ */
+
 
 class Point {
     int x;
@@ -20,9 +26,8 @@ class MazeBFS {
     static int arr[][], dis[][];
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    static int answer = 0;
 
-    public int BFS(int x, int y, int[][] arr) {
+    public int BFS(int x, int y) {
         Queue<Point> q = new LinkedList();
         q.offer(new Point(x,y));
         //q에 들어가 있는 상태라면 1로 표시하기.
@@ -30,29 +35,35 @@ class MazeBFS {
 
         while(!q.isEmpty()) {
             Point tmp = q.poll();
-            for (int i=0;i<4;i++) {
-                int nx = tmp.x+dx[i];
+            for(int i=0;i<4;i++) {
+                int nx = tmp.x + dx[i];
                 int ny = tmp.y + dy[i];
-                if(nx>=1 && nx<=7 && ny>=1 && ny<=7 && arr[nx][ny] ==0) {
+                if(nx >=1&&nx<=7&&ny >=1&&ny<=7&&arr[nx][ny]==0) {
                     arr[nx][ny] = 1;
                     q.offer(new Point(nx, ny));
-                    dis[nx][ny] = dis[tmp.x][tmp.y]+1;
+                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
                 }
             }
-        }
+            }
 
-        return answer;
+        if(dis[7][7]==0) {
+            return -1;
+        } else {
+            return dis[7][7];
+        }
     }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MazeBFS T = new MazeBFS();
-        int [][] arr = new int[8][8];
+        arr = new int[8][8];
+        dis = new int[8][8];
         for (int i=1; i<8;i++) {
             for(int j=1;j<8;j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-        System.out.println(T.BFS(1,1, arr));
+        System.out.println(T.BFS(1,1));
     }
 }
