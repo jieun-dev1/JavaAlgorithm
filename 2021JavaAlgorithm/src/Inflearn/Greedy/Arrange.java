7b4226faff07166e5ee92bc9b762724b93759d04
jieun-dev1 <jkdev1324@gmail.com>
2022-07-08 16:13:09 +0900

    Greedy 2차구현

--- 2021JavaAlgorithm/src/Inflearn/Greedy/Arrange.java
+++ 2021JavaAlgorithm/src/Inflearn/Greedy/Arrange.java
@@ -5,7 +5,11 @@ import java.util.Collections;
 import java.util.Scanner;
 
 /**
- * (1) 우선은 오름차순으로 정렬
+ * 2차 시도.
+ * Greedy:
+ * (1) 회의가 종료 시간을 오름차순으로 정렬한다.
+ * (2) 끝나는 시간이 동일하다면 시작 시간을 오름차순 정렬.
+ * 이중 for 문이 아니어도 되는 듯함.
  */
 
 class Meeting implements Comparable<Meeting> {
@@ -19,43 +23,33 @@ class Meeting implements Comparable<Meeting> {
 
     @Override
     public int compareTo(Meeting o) {
-        if(this.startAt == o.startAt) {
-            return this.endAt - o.endAt;
-        } else {
+        if(this.endAt == o.endAt) {
             return this.startAt - o.startAt;
+        } else {
+            return this.endAt - o.endAt;
         }
     }
 }
+
 class Arrange {
 
     public int solution(ArrayList<Meeting> arr, int cnt) {
-        ArrayList<Integer> answerList = new ArrayList<>();
-//        int answer=0; //여기 선언하면 answer 이 이전 인덱스 값에서 추가됨.
-        for(int i=0; i<cnt-1;i++) {
-            int answer=1; //무조건 해당 회의실 1개는 잡히니까.
-            int tmp = arr.get(i).endAt;
+        int tmp = 0;
+        int answer = 0;
 
-            for(int j=i+1;j<cnt;j++) {
-//                int tmp = arr.get(i).endAt; //실수: 여기 선언하면 endAt에서 업데이트 한 걸 다시 덮는 격.
-                if(tmp <= arr.get(j).startAt) {
-                    tmp = arr.get(j).endAt;
-                    answer ++;
-                }
-
-                else {
-                    continue;
-                }
+        for (Meeting o : arr) {
+            if (o.startAt >= tmp) {
+                tmp = o.endAt;
+                answer++;
             }
-            answerList.add(answer);
         }
-
-        return Collections.max(answerList);
+        return answer;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Arrange T = new Arrange();
         ArrayList<Meeting> arr = new ArrayList<>();
+        Arrange T = new Arrange();
         int cnt = sc.nextInt();
         for(int i=0;i<cnt;i++) {
             int start = sc.nextInt();
