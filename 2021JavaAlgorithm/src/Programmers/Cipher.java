37ac416f75ce835a80d66c6bf4b35095d1b9d854
jieun-dev1 <jkdev1324@gmail.com>
2022-07-22 13:51:05 +0900

    2차 시도: ArrayList로 변경해서 해결 - 정답

--- 2021JavaAlgorithm/src/Programmers/Cipher.java
+++ 2021JavaAlgorithm/src/Programmers/Cipher.java
@@ -18,20 +18,56 @@ char 는, 'c'-2 = 2; 로 계싼이 된다. char타입은 문자가 저장된다기보다, 2byte크기�
 
 public class Cipher {
     public String solution(String s, int n) {
-        String answer = "";
-        n = n%26; //26이 넘을 때, -26을 해주는 것보다 나머지로 계산하기.
+//        String answer = "";
+        ArrayList<Character> upper = new ArrayList<>();
+        ArrayList<Character> lower = new ArrayList<>();
 
-//        char[] arr = s.toCharArray();
-        for(int i=0;i<s.length();i++) {
-            char temp = s.charAt(i); // String 을 charArray로 바꾸지 않고도, charAt을 쓰면 특정 인덱스의 Character 반환 가능.
-            if(Character.isLowerCase(temp)) {
-                temp = (char) ((temp -'a'+n) % 26 +'a');
-            } else if(Character.isUpperCase(temp)){
-                temp = (char) ((temp -'A'+n) % 26 +'A');
+        //['A',....'Z']
+        for (char i ='A'; i<='Z';i++) {
+            upper.add(i);
+        }
+        //['a',....'z']
+        for (char i ='a'; i<='z';i++) {
+            lower.add(i);
+        }
+
+        //주어진 String 을 배열로 변환.
+        ArrayList<Character> answerChar = new ArrayList<>(); //길이가 8000까지 있는데 어떤 자료구조를 만들지?
+        char[] givenChar = s.toCharArray();
+
+        //char[Array]의 i 번째 원소를 찾고.
+        //if 소문자라면, lower를 / 대문자라면 upper 참조
+        //charArray에서 주어진 값의 upper 배열 내 위치를 찾는다. 인덱스를 n 추가해서 다시 할당한다.
+        //만약 n+인덱스가 26이상이라면, 26을 빼서 반환한다.
+        for(int i=0;i<givenChar.length;i++) {
+            //현재 인덱스부터 찾기 (temp)
+            if(Character.isUpperCase(givenChar[i])) {
+                int temp = upper.indexOf(givenChar[i]);
+                if(temp+n<26) {
+                    answerChar.add(upper.get(temp+n));
+                } else {
+                    answerChar.add(upper.get(temp+n-26));
+                }
             }
-            answer += temp; //공백일 경우 분기처리 없이 그대로 더해주자.
+//
+            else if (Character.isWhitespace(givenChar[i])) {
+                answerChar.add(givenChar[i]);
+            }
+            else {
+                int temp = lower.indexOf(givenChar[i]);
+                if(temp+n<26) {
+                    answerChar.add(lower.get(temp+n));
+                } else {
+                    answerChar.add(lower.get(temp+n-26));
+                }
+            }
         }
+        String answer = "";
+        for(int i=0;i<answerChar.size();i++){
+            answer += answerChar.get(i);
+        }
+        //char 배열을 String 으로 변환해서 반환.
         return answer;
-
-        }
     }
+}
+
