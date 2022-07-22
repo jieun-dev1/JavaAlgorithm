b3418a6a816f1754a8b56da1269748ff36c55c15
jieun-dev1 <jkdev1324@gmail.com>
2022-07-22 14:25:03 +0900

    핸드폰 번호 가리기

---
+++ 2021JavaAlgorithm/src/Programmers/Prac/Masking.java
@@ -0,0 +1,18 @@
+package Programmers.Prac;
+
+public class Masking {
+
+    public String solution(String phone_number) {
+        String answer = "";
+        for (int i = 0; i < phone_number.length(); i++) {
+            if (i < phone_number.length() - 4) {
+                answer += "*";
+            } else {
+                answer += phone_number.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+}
+
