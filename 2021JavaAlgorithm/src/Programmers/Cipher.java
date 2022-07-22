77dabb53054c49bf45cde66f9de4527a136256f3
jieun-dev1 <jkdev1324@gmail.com>
2022-07-22 13:49:06 +0900

    3차 시도: 아스키코드  프로그래머스 답안

--- 2021JavaAlgorithm/src/Programmers/Cipher.java
+++ 2021JavaAlgorithm/src/Programmers/Cipher.java
@@ -4,66 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /*
-1. 알파벳이 담긴 배열을 만든다. //알파벳은 총 26개이다.
-2. 어떤 자료 구조? ArrayList 와 일반 배열 차이: arraylist는 배열의 길이를 조정 가능.
-3. n 만큼 인덱스를 밀어서, 원소를 바꾸고, 다시 result 로 출력한다.
-4. 공백은 continue로 다음을 읽는다.
-5. z-> a 같은 경우는, 주어진 길이가 24였따면,
- */
+3차: 프로그래머스 정답
+(1) 활용: 아스키 코드
+아스키 코드를 구하고 (char)를 해주면 다시 캐릭터 변환이 가능하다.
+char 는, 'c'-2 = 2; 로 계싼이 된다. char타입은 문자가 저장된다기보다, 2byte크기의 양의정수를 저장한다
+(대응하는 유니코드 문자가 출력한다고 생각하기)
+
+연산할 때도, 정수값이 사용된다. https://devlog-wjdrbs96.tistory.com/76
+'ㅁ' sms
+(2) 스트링은 += 로 더해줘도 된다.
+*/
+
+
 public class Cipher {
-
     public String solution(String s, int n) {
-//        String answer = "";
-        char[] upper = new char[26];
-        char[] lower = new char[26];
+        String answer = "";
+        n = n%26; //26이 넘을 때, -26을 해주는 것보다 나머지로 계산하기.
 
-        //['A',....'Z']
-        for (char i ='A'; i<='Z';i++) {
-            for(char j=0;j<26;j++) {
-                upper[j] = i;
+//        char[] arr = s.toCharArray();
+        for(int i=0;i<s.length();i++) {
+            char temp = s.charAt(i); // String 을 charArray로 바꾸지 않고도, charAt을 쓰면 특정 인덱스의 Character 반환 가능.
+            if(Character.isLowerCase(temp)) {
+                temp = (char) ((temp -'a'+n) % 26 +'a');
+            } else if(Character.isUpperCase(temp)){
+                temp = (char) ((temp -'A'+n) % 26 +'A');
             }
+            answer += temp; //공백일 경우 분기처리 없이 그대로 더해주자.
         }
-        //['a',....'z']
-        for (char i ='a'; i<='z';i++) {
-            for(char j=0;j<26;j++) {
-                lower[j] = i;
-            }
-        }
-
-        //주어진 String 을 배열로 변환.
-        ArrayList<Character> answerChar = new ArrayList<>(); //길이가 8000까지 있는데 어떤 자료구조를 만들지?
-        char[] givenChar = s.toCharArray();
-
-        //char[Array]의 i 번째 원소를 찾고.
-        //if 소문자라면, lower를 / 대문자라면 upper 참조
-        //charArray에서 주어진 값의 upper 배열 내 위치를 찾는다. 인덱스를 n 추가해서 다시 할당한다.
-        //만약 n+인덱스가 26이상이라면, 26을 빼서 반환한다.
-        for(int i=0;i<givenChar.length;i++) {
-            //현재 인덱스부터 찾기 (temp)
-            if(Character.isUpperCase(givenChar[i])) {
-                int temp = Arrays.asList(upper).indexOf(givenChar[i]);
-                if(temp+n<26) {
-                    answerChar.add(upper[temp+n]);
-                } else {
-                    answerChar.add(upper[temp+n-26]);
-                }
-            }
-//
-//            else if (Character.isWhitespace(givenChar[i])) {
-//                answerChar.add(givenChar[i]);
-//            }
-            else {
-                int temp = Arrays.asList(lower).indexOf(givenChar[i]);
-                if(temp+n<26) {
-                    answerChar.add(lower[temp+n]);
-                } else {
-                    answerChar.add(lower[temp+n-26]);
-                }
-            }
-        }
-        String answer = answerChar.toString();
-        //char 배열을 String 으로 변환해서 반환.
         return answer;
-    }
 
-}
+        }
+    }
--- 2021JavaAlgorithm/src/Test/CipherTest.java
+++ 2021JavaAlgorithm/src/Test/CipherTest.java
@@ -18,7 +18,26 @@ public class CipherTest {
         String actual = this.cipher.solution(s, n) ;
         String expected = "BC";
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void Test2() {
+        String s = "z";
+        int n = 1;
+        String actual = this.cipher.solution(s, n) ;
+        String expected = "a";
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test3() {
+        String s = "a B z";
+        int n = 4;
+        String actual = this.cipher.solution(s, n) ;
+        String expected = "e F d";
+
+        Assert.assertEquals(expected, actual);
+    }
 }
