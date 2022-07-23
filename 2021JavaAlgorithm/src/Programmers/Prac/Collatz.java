1d99638dd08de9d95ff5997a2912fbf229bb3d4a
jieun-dev1 <jkdev1324@gmail.com>
2022-07-23 16:38:29 +0900

    int -> long 추가

--- 2021JavaAlgorithm/src/Programmers/Prac/Collatz.java
+++ 2021JavaAlgorithm/src/Programmers/Prac/Collatz.java
@@ -2,27 +2,31 @@ package Programmers.Prac;
 
 import java.util.Scanner;
 
+//n에 3을 곱하는 걸 생각할 때, int 가 아닌 long변환이 맞다.
+//int범위: ?2,147,483,648 ~ 2,147,483,647
+//long 범위:223,372,036,854,775,808 ~ 223,372,036,854,775,807
 public class Collatz {
         public int solution(int num) {
             int answer = 0;
             //1이 주어졌을 떄.
-            if (num == 1) {
+            long n = (long) num;
+            if (n == 1) {
                 return answer;
             } else {
                 //1이 주어지지 않았을 때.
                 for(int i=0;i<500;i++) {
-                    if (num == 1) {
+                    if (n == 1) {
                         return answer;
                     }
-                    else if((num%2) ==0) {
-                        num = num/2;
-                    } else if ((num%2) ==1) {
-                        num = (num*3) + 1;
+                    else if((n%2) ==0) {
+                        n = n/2;
+                    } else if ((n%2) ==1) {
+                        n = (n*3) + 1;
                     }
                     answer++;
                 }
 
-                if(num!=1) {
+                if(n!=1) {
                     answer = -1;
                 }
             }
