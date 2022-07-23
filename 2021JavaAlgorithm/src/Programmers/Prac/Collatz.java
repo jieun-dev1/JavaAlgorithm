be71c5e1fe999ab33aef2314bfb735f1a6977e2f
jieun-dev1 <jkdev1324@gmail.com>
2022-07-23 16:34:10 +0900

    삼항 연산자 없이 품.

--- 2021JavaAlgorithm/src/Programmers/Prac/Collatz.java
+++ 2021JavaAlgorithm/src/Programmers/Prac/Collatz.java
@@ -2,18 +2,33 @@ package Programmers.Prac;
 
 import java.util.Scanner;
 
-//삼항 연산자를 사용한 풀이
 public class Collatz {
-    //num이 8000000 미만인 정수이기 때문에, long 으로 변환한 것 같다.
+        public int solution(int num) {
+            int answer = 0;
+            //1이 주어졌을 떄.
+            if (num == 1) {
+                return answer;
+            } else {
+                //1이 주어지지 않았을 때.
+                for(int i=0;i<500;i++) {
+                    if (num == 1) {
+                        return answer;
+                    }
+                    else if((num%2) ==0) {
+                        num = num/2;
+                    } else if ((num%2) ==1) {
+                        num = (num*3) + 1;
+                    }
+                    answer++;
+                }
 
-    public int solution(int num) {
-        long n = (long)num;
-        for(int i=0;i<500;i++) {
-            if(n == 1) return i; //1이 될때까지의 횟수 == 반복문을 돌린 횟수 //return 은 반복문 포함된 메스드를 종료함.
-            n = (n%2 == 0) ? n/2 :n*3+1;
+                if(num!=1) {
+                    answer = -1;
+                }
+            }
+
+            return answer;
         }
-        return -1; //에 해당하지 않는다면, -1을 반환.
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
