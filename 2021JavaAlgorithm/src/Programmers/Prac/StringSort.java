f4331ac19c1895a45854d93bfac7ddc2af874734
jieun-dev1 <jkdev1324@gmail.com>
2022-07-24 16:33:04 +0900

    문자열 조작
    Collections.sort 로 정렬하기, Substring 사용.
    인덱스하는 글자를 앞으로 빼와도, 전체 정렬에는 큰 지장 없다는 게 핵심.

---
+++ 2021JavaAlgorithm/src/Programmers/Prac/StringSort.java
@@ -0,0 +1,34 @@
+package Programmers.Prac;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+Arrays.sort는 기본적으로 사전 순으로 나열된다.
+풀이 어려워서 답 봄.
+
+특정 n 번째 인덱스를 앞으로 가져와서 정렬하기.
+Collections.sort = Natural Ordering (alphabet 순,
+
+ */
+public class StringSort {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i=0;i<strings.length;i++) {
+            arr.add(strings[i].charAt(n)+strings[i]);
+        }
+
+        Collections.sort(arr);
+        //추가한 정렬 인덱스 빼서 리턴하기.
+        answer = new String[arr.size()];
+
+        for(int i=0;i<arr.size();i++) {
+            answer[i] = arr.get(i).substring(1);
+        }
+
+        return answer;
+    }
+}
+
