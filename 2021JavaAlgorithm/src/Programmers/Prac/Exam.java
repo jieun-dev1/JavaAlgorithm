90346b25e953f4dbc9e709aaa4b9bc84b2850dc5
jieun-dev1 <jkdev1324@gmail.com>
2022-07-24 19:40:04 +0900

    완전탐색 2번째 답 stream으로 list를 Array로 변환 Math.max 를 한 번 거쳐서 비교하면, 3개 중 최대값 찾을 수 있음

--- 2021JavaAlgorithm/src/Programmers/Prac/Exam.java
+++ 2021JavaAlgorithm/src/Programmers/Prac/Exam.java
@@ -1,57 +1,44 @@
 package Programmers.Prac;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/*
+player 1,2,3을 int 로 주지 말고 풀어보자.
+ */
 public class Exam {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        ArrayList<Integer> answerArray = new ArrayList<>();
         int[] arrOne  = {1,2,3,4,5};
         int[] arrTwo = {2,1,2,3,2,4,2,5};
         int[] arrThree = {3,3,1,1,2,2,4,4,5,5};
-
-        //수포자별 맞춘 문제 개수 초기화
-        int player1 = 0;
-        int player2 = 0;
-        int player3 = 0;
+        int[] score = new int[3];
 
         //주어진 문제의 답을 비교하기.
         for(int i=0;i<answers.length;i++){
             if(answers[i]==arrOne[i%5]) {
-                player1++;
+                score[0]++;
             }
             if (answers[i]==arrTwo[i%8]) {
-                player2++;
+                score[1]++;
             }
             if (answers[i]==arrThree[i%10]) {
-                player3++;
+                score[2]++;
             }
         }
 
-        //list에 각 수포자들의 맞춘 갯수 넣기.
+        //가장 많이 맞춘 수
+
+        int max = Math.max(score[0],  Math.max(score[1],score[2])); //score 0,1,2 중에 max 와 같은 값이 있다면, + 해주기.
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(player1);
-        list.add(player2);
-        list.add(player3);
-
-        //가장 많이 맞춘 개수가 앞으로 오게 역순 정렬
-//        Collections.reverse(list);
-        Collections.sort(list, Collections.reverseOrder());
-        //가장 큰 수가 = 수포자 1,2,3 중에 있다면, answerArray 최종에 더하기.
-        if(list.get(0) == player1) answerArray.add(1);
-        if(list.get(0) == player2) answerArray.add(2);
-        if(list.get(0) == player3) answerArray.add(3);
-
-        //배열로 반환하기.
-        answer = new int[answerArray.size()];
-
-        for(int i=0;i<answerArray.size();i++) {
-            answer[i] = answerArray.get(i);
+        if(max == score[0]) {
+            list.add(1);
         }
-
-        return answer;
+        if(max == score[1]) {
+            list.add(2);
+        }
+        if(max == score[2]) {
+            list.add(3);
+        }
+        //list.stream().toArray(); 이렇게만 하면 Object[] 형이다.
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
 }
