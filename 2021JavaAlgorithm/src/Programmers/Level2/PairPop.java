44f30c87f773c48dc2dbd2e3b0d50d860a8d527b
jieun-dev1 <jkdev1324@gmail.com>
2022-09-30 20:42:01 +0900

    Queue 활용으로 해결

---
+++ 2021JavaAlgorithm/src/Programmers/Level2/PairPop.java
@@ -0,0 +1,29 @@
+package Programmers.Level2;
+
+import java.util.Stack;
+
+public class PairPop {
+  public int solution(String s) {
+    char[] arr = s.toCharArray();
+    Stack<Character> stack = new Stack<Character>();
+    int answer = 0;
+    for(int i=0;i<arr.length;i++) { //
+      if(stack.isEmpty()){
+        stack.push(arr[i]);
+        continue;
+      } else if((!stack.isEmpty())&&(stack.peek()==arr[i])){
+        stack.pop();
+        continue;
+      } else if((!stack.isEmpty())&&(stack.peek()!=arr[i])){
+        stack.push(arr[i]);
+      }
+    }
+
+    if(stack.isEmpty()) //배열을 다 순회하고 나서,
+    { answer = 1;
+    } else {
+      answer = 0;
+    }
+    return answer;
+  }
+}
