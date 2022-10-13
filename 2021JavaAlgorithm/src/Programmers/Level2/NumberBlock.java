ef87a3a80978744e6e0a7ac2990ddc2ca498e5e4
jieun-dev1 <jkdev1324@gmail.com>
2022-10-13 14:01:46 +0900

    테스트 맞았으나 효율성 테스트 시간 초과. 10000000 제약 조건 추가해도 시간 초과

--- 2021JavaAlgorithm/src/Programmers/Level2/NumberBlock.java
+++ 2021JavaAlgorithm/src/Programmers/Level2/NumberBlock.java
@@ -4,27 +4,34 @@ package Programmers.Level2;
 /**
  * end-begin이 10,000을 넘지 않기 때문에 int[] answer의 길이는 10,000 넘지 않음.
  * 자기 자신이 아닌 약수 중 가장 큰 약수.
+ *
+ * double을 int로 변환하면 내림으로 계산.
+ * 제곱근이 정수가 아니라면, 어차피 상관 없음.
  */
 public class NumberBlock {
 
   public int[] solution(long begin, long end) {
-
-
+    //int 배열로 반환해야 하니, 초기에 long을 int 로 반환해주기.
+    int beginPoint = (int) begin;
+    int endPoint = (int) end;
     //1 - end 까지를 만들고, begin-end 는 따로.
-    int answer[] = new int[(int) end-(int) begin+1]; //
+    int answer[] = new int[endPoint-beginPoint+1]; //
 
-    for(long i=begin;i<end+1;i++) {
-      if(i%2 == 0) {
-        answer[((int)i-(int)begin)] = (int)i/2; ///2의 배수인 경우 2로 나눈 값이 약수 중 가장 큰 수. 2의 배수면 순회할 필요 x.
-      } else {
-        for(long j=(int)i/2;j>=1;j--) {
-          if(i%j==0) {
-            answer[((int)i-(int)begin)] = (int) j;
+    for(int i=beginPoint;i<endPoint+1;i++) {
+        for(int j=i/2;j>=(int)Math.sqrt(i);j--) {
+          if(j>=10000000) {
+            continue;
+          }
+          else if((j<=10000000)&& (i%j==0)) {
+            answer[(i-beginPoint)] = j;
+            break;
+          }
+          else  if ((j<=10000000) && (j==(int) Math.sqrt(i)) && (i%j!=0)) {
+            answer[(i-beginPoint)] = 1; //제곱근으로 나눠지지 않는다면,
             break;
           }
         }
       }
-    }
     return answer;
   }
 }
