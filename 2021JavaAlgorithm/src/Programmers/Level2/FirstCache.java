34354b35f5ec9451ceb8b86ab747fc08dcea939b
jieun-dev1 <jkdev1324@gmail.com>
2022-10-30 12:40:01 +0900

    인덱스 위치 삭제가 핵심

--- 2021JavaAlgorithm/src/Programmers/Level2/FirstCache.java
+++ 2021JavaAlgorithm/src/Programmers/Level2/FirstCache.java
@@ -1,17 +1,18 @@
 package Programmers.Level2;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 /**
- * ArrayList는 더할 떄 끝에 더해줌.
- * Queue의 Size 측정하는 이유: Queue가 cacheSize보다 짧다면 offer만 해도 되지만, cacheSize와 같으면 기존 원소 poll, offer() 과정.
+ * ArrayList는 더할 떄 끝에 더해줌. Queue의 Size 측정하는 이유: Queue가 cacheSize보다 짧다면 offer만 해도 되지만, cacheSize와 같으면
+ * 기존 원소 poll, offer() 과정. 삽입된 데이터를 사용하면, HEAD 로 옮겨서 우선순위를 높이게 되고 삭제될 우선순위에서 멀어짐. Queue가 아닌
+ * LinkedList로 구현해야, indexOf를 할 수 있음. 처음에 부모를 Queue로 선언했는데, 이렇게 하면 사용 가능한 메서드가 제한됨.
+ *
  */
 public class FirstCache {
 
   public int solution(int cacheSize, String[] cities) {
     int answer = 0;
-    Queue<String> queue = new LinkedList<>();
+    LinkedList<String> queue = new LinkedList<>();
     for (int i = 0; i < cities.length; i++) {
       cities[i] = cities[i].toUpperCase();
     }
@@ -27,17 +28,22 @@ public class FirstCache {
         queue.offer(cities[i]);
         answer += 5;
       }
-
-      else if (queue.contains(cities[i]) && queue.size() <= cacheSize) {
+      //cache hit
+      else if (queue.contains(cities[i]) && queue.peek() != cities[i]
+          && queue.size() <= cacheSize) {
+        int temp = queue.indexOf(cities[i]);
+        queue.remove(temp);
+        queue.offer(cities[i]);
         answer += 1;
       }
-
-      //캐시에 없을 경우 맨뒤의 원소를 제거하고 맨 앞의 원소 추가함.
+      //cache miss  - 캐시에 없을 경우 맨뒤의 원소를 제거하고 맨 앞의 원소 추가함.
       else if (!queue.contains(cities[i]) && queue.size() == cacheSize) {
         queue.poll();
         queue.offer(cities[i]);
         answer += 5;
       }
+
+
     }
 
     return answer;
@@ -45,8 +51,8 @@ public class FirstCache {
 
   public static void main(String[] args) {
     FirstCache cache = new FirstCache();
-    System.out.println(cache.solution(2, new String[]{"a", "a", "a", "b", "b", "b", "c", "c", "c"}));
-//    cache.solution(2, new String[]{"a", "a", "a", "b", "b", "b", "c", "c", "c"});
+    System.out.println(
+        cache.solution(2, new String[]{"a", "a", "a", "b", "b", "b", "c", "c", "c"}));
 
   }
 
