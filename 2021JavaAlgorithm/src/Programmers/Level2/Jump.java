1ec0a3ca28dd59556d6d290649edcc613e6bfab1
jieun-dev1 <jkdev1324@gmail.com>
2022-11-20 23:15:33 +0900

    피보나치 문제

--- 2021JavaAlgorithm/src/Programmers/Level2/Jump.java
+++ 2021JavaAlgorithm/src/Programmers/Level2/Jump.java
@@ -1,30 +1,36 @@
 package Programmers.Level2;
 
+//2000을 넣어서 1234567 나머지를 구하면 -236727이 나오기에 미리 1234567 나눠서 넣어줘야 함.
+
+
+// * 숫자 A, b, c가 있을 때, (A+B)%C 의 값은 ((A%C)+(B%C))%C 와 같다.
 public class Jump {
 
-  static int n;
-  static long answer = 0;
-  static int[] ch = new int[]{1,2}; //0번째 원소 -1 , 첫 번째 원소 - 2로 생각.
+  static int[] fibo;
 
   public long solution(int n) {
-    this.n = n;
-    DFS(0, ch[0]);
-    DFS(0, ch[1]);
-    return answer%1234567;
+    fibo = new int[n + 1];
+    if (n <= 2) {
+      fibo[n] = n;
+
+    }
+//    if (n==1){
+//      fibo[n] = 1%1234567;
+//    } else if (n==2) {
+//      fibo[n] = 2%1234567;
+//    }
+
+    else if (n >= 3) {
+      fibo[1] = 1 % 1234567;
+      fibo[2] = 2 % 1234567;
+      for (int i = 3; i <= n; i++) {
+        fibo[i] = (fibo[i - 2] + fibo[i - 1]) % 1234567;
+      }
+    }
+
+    return fibo[n];
   }
 
-  public void DFS(int sum, int current){
-    sum = sum+current;
-    if(sum == n) {
-      answer++;
-      return;
-    } else if(sum>n) {
-      return;
-    } else if (sum<n){
-      DFS(sum, ch[0]);
-      DFS(sum, ch[1]);
-    }
-  }
 
   public static void main(String[] args) {
     Jump T = new Jump();
