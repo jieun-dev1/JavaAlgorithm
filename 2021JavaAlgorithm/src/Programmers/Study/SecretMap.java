ac1ec0cb0fda58fca944ed9c441a05c83271cbc9
jieun-dev1 <jkdev1324@gmail.com>
2022-11-23 21:42:40 +0900

    메서드 쪼개서 테스트 통과

--- 2021JavaAlgorithm/src/Programmers/Study/SecretMap.java
+++ 2021JavaAlgorithm/src/Programmers/Study/SecretMap.java
@@ -5,46 +5,22 @@ package Programmers.Study;
  * 1. arr1 과 arr2가 반복되는 코드를 어떻게 줄이지?
  */
 public class SecretMap {
+
   public String[] solution(int n, int[] arr1, int[] arr2) {
     /**
      * 숫자를 2진수로 변환한
      */
-    int[][] binArr1 = new int[n][n];
-    int[][] binArr2 = new int[n][n];
     char[][] answerArr = new char[n][n];
     String[] stringAnswer = new String[n];
+
+    int[][] binArr1 = convertToBinary(arr1, n);
+    int[][] binArr2 = convertToBinary(arr2, n);
 
     /**
      * 이중 for 문 내부에서 - for문 2개를 거침 2X16X16  +16X16+16X16 = 16X16X4 = 1024;
      */
-    for(int i=0;i<n;i++){
-      String s = Integer.toString(arr1[i],2);
-      if(s.length()<n){
-        int tempLen = n - s.length();
-        StringBuffer tempS = new StringBuffer(s); //1001 -> 01001
-        for(int j=0;j<tempLen;j++){
-          tempS.insert(0,"0");
-        }
-        s = tempS.toString();
-      }
-      for(int j=0;j<n;j++){
-        binArr1[i][j] = s.charAt(j)-'0';
-      }
 
-      String s2 = Integer.toString(arr2[i],2);
-      if(s2.length()<n){
-        int tempLen = n - s2.length();
-        StringBuffer tempS = new StringBuffer(s2);
-        for(int j=0;j<tempLen;j++){
-          tempS.insert(0,"0");
-        }
-        s2 = tempS.toString();
-      }
-      for(int j=0;j<n;j++){
-        binArr2[i][j] = s2.charAt(j)-'0';
-      }
-    }
-
+    // 하나라도 1이 나오면 answerArr는 1이 되는 것.
     for(int i=0;i<n;i++){
       for(int j=0;j<n;j++){
         if(binArr1[i][j]==1 || binArr2[i][j]==1 ){
@@ -53,6 +29,7 @@ public class SecretMap {
       }
     }
 
+    // # 혹은 공백으로 채워주기.
     for(int i=0;i<n;i++){
       StringBuilder s = new StringBuilder();
       for(int j=0;j<n;j++){
@@ -68,6 +45,26 @@ public class SecretMap {
     }
     return stringAnswer;
   }
+
+  public int[][] convertToBinary(int[] arr, int n){
+    int[][] intArr  = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      String s = Integer.toString(arr[i], 2);
+      if (s.length() < n) {
+        int tempLen = n - s.length();
+        StringBuffer tempS = new StringBuffer(s); //1001 -> 01001
+        for (int j = 0; j < tempLen; j++) {
+          tempS.insert(0, "0");
+        }
+        s = tempS.toString();
+      }
+      for (int j = 0; j < n; j++) {
+        intArr[i][j] = s.charAt(j) - '0';
+      }
+    }
+    return intArr;
+  }
+
 
   public static void main(String[] args) {
     SecretMap T = new SecretMap();
