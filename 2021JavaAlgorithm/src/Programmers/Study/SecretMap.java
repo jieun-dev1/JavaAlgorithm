a49d0597b02707e54fde0fa62e0635186275dc98
jieun-dev1 <jkdev1324@gmail.com>
2022-11-23 20:44:32 +0900

    정답코드지만 중복이 많음

---
+++ 2021JavaAlgorithm/src/Programmers/Study/SecretMap.java
@@ -0,0 +1,76 @@
+package Programmers.Study;
+
+/**
+ * 풀이 시간: 40시간
+ * 1. arr1 과 arr2가 반복되는 코드를 어떻게 줄이지?
+ */
+public class SecretMap {
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+    /**
+     * 숫자를 2진수로 변환한
+     */
+    int[][] binArr1 = new int[n][n];
+    int[][] binArr2 = new int[n][n];
+    char[][] answerArr = new char[n][n];
+    String[] stringAnswer = new String[n];
+
+    /**
+     * 이중 for 문 내부에서 - for문 2개를 거침 2X16X16  +16X16+16X16 = 16X16X4 = 1024;
+     */
+    for(int i=0;i<n;i++){
+      String s = Integer.toString(arr1[i],2);
+      if(s.length()<n){
+        int tempLen = n - s.length();
+        StringBuffer tempS = new StringBuffer(s); //1001 -> 01001
+        for(int j=0;j<tempLen;j++){
+          tempS.insert(0,"0");
+        }
+        s = tempS.toString();
+      }
+      for(int j=0;j<n;j++){
+        binArr1[i][j] = s.charAt(j)-'0';
+      }
+
+      String s2 = Integer.toString(arr2[i],2);
+      if(s2.length()<n){
+        int tempLen = n - s2.length();
+        StringBuffer tempS = new StringBuffer(s2);
+        for(int j=0;j<tempLen;j++){
+          tempS.insert(0,"0");
+        }
+        s2 = tempS.toString();
+      }
+      for(int j=0;j<n;j++){
+        binArr2[i][j] = s2.charAt(j)-'0';
+      }
+    }
+
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(binArr1[i][j]==1 || binArr2[i][j]==1 ){
+          answerArr[i][j] = 1;
+        }
+      }
+    }
+
+    for(int i=0;i<n;i++){
+      StringBuilder s = new StringBuilder();
+      for(int j=0;j<n;j++){
+        //한 행마다 s를 갖는다.
+        if(answerArr[i][j]==1){
+          answerArr[i][j] = '#';
+        } else {
+          answerArr[i][j] = ' ';
+        }
+        s.append(answerArr[i][j]);
+      }
+      stringAnswer[i] = s.toString();
+    }
+    return stringAnswer;
+  }
+
+  public static void main(String[] args) {
+    SecretMap T = new SecretMap();
+    System.out.println(T.solution(5, new int[]{9,20,28,18,11},new int[]{30, 1, 21, 17, 28}));
+  }
+}
