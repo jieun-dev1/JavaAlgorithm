8ddee71c49591a453c8c0727281ecef16fc5faaf
jieun-dev1 <jkdev1324@gmail.com>
2022-12-26 15:50:42 +0900

    그리디 - 정답
    오답 해결
    
    (1) indexOutOfRange 처리
    1이나 5처럼 맨 처음/끝에 있을 때는 왼쪽, 오른쪽 처리가 불가하다.
    왼쪽 오른쪽이 0이상이도록 해야함 (여기서는 1과 n 사이)
    
    (2) 코드 가독성
    길더라도 check 배열은 check 만 하는 용도니까, 숫자 그대로 체크할 수 있게, 실제 길이보다 +1 해서 사이즈 만들기.
    
    (3) 인덱스를 1씩 추가하니까 (숫자를  그대로 ) 코드 읽으면서 헷갈렸다.
    앞 뒤, 를 체크한다면 -1, +1 이어야 한다는 걸 생각해봊.  처음에 마지막 else if 블록은 spareArr[i]이라고 함. 여기는 [i]+1 이어야 한다.
    
    *헷갈릴 때는, 직접 예제 대입/ 코드 주석달기.

--- 2021JavaAlgorithm/src/BaekJoon/Greedy/BJ2891.java
+++ 2021JavaAlgorithm/src/BaekJoon/Greedy/BJ2891.java
@@ -9,25 +9,25 @@ import java.util.StringTokenizer;
 
 public class BJ2891 {
 
-  public int solution(int cnt, int[] dmgArr,int[] moreArr){
-    int answer = dmgArr.length;//시작 값: 전체 팀 - 손상된 팀.
-    int[] cntArr = new int[cnt];
-    for(int i=0;i<dmgArr.length;i++){
-      cntArr[dmgArr[i]-1]-=1;
+  public int solution(int cnt, int[] damagedArr,int[] spareArr){
+    int answer = damagedArr.length;//시작 값: 전체 팀 - 손상된 팀.
+    int[] check = new int[cnt+1]; //전체 팀 중 손상된 팀의 번호를 체크하는 배열. 인덱스 처리가 편리하게, +1로 함.
+    for(int i=0;i<damagedArr.length;i++){
+      check[damagedArr[i]]=1; //1이라면 손상된 것. check배열은 0부터이니, 1을 빼줘야 함.
     }
-    for(int i=0;i<moreArr.length;i++){
-      cntArr[moreArr[i]-1]+=1;
-    }
-    for(int i=0;i<cntArr.length;i++){
-      if(cntArr[i]==1){
-        if(i+1<=4&&cntArr[i+1]==-1){
-          cntArr[i+1]+=1;
-          cntArr[i]-=1;
-          answer--;//복구가 될 때
-        } else if(i-1>=0&&cntArr[i-1]==-1) {
-          cntArr[i-1]+=1;
-          cntArr[i]-=1;
-          answer--;//복구가 될 때
+    for(int i=0;i<spareArr.length;i++){
+      //해당 인덱스가 check 되어 있는지 확인.
+      if(check[spareArr[i]]==1){ //손상된 팀 == 여분의 보트 가지고 있는 팀.
+        check[spareArr[i]]=0; //손상 회복.
+        answer--;
+      } else {
+        //해당 인덱스가 uncheck 라면, 앞뒤 확인.
+        if(spareArr[i]-1>=1&&check[spareArr[i]-1]==1){
+          check[spareArr[i]-1]=0;
+          answer--;
+        } else if(spareArr[i]+1<=cnt&&check[spareArr[i]+1]==1){
+          check[spareArr[i]+1]=0;
+          answer--;
         }
       }
     }
@@ -44,18 +44,18 @@ public class BJ2891 {
     int team = Integer.parseInt(st.nextToken()); //총 팀의 수
     int damaged = Integer.parseInt(st.nextToken()); //손상된 팀의 수
     int spare = Integer.parseInt(st.nextToken());
-    int[] dmgArr = new int[damaged];
-    int[] moreArr = new int[spare];
+    int[] damagedArr = new int[damaged];
+    int[] spareArr = new int[spare];
     st = new StringTokenizer(br.readLine());
     for(int i=0;i<damaged;i++){
-      dmgArr[i] = Integer.parseInt(st.nextToken());
+      damagedArr[i] = Integer.parseInt(st.nextToken());
     }
 
     st = new StringTokenizer(br.readLine());
     for(int i=0;i<spare;i++){
-      moreArr[i] = Integer.parseInt(st.nextToken());
+      spareArr[i] = Integer.parseInt(st.nextToken());
     }
-    sb.append(T.solution(team, dmgArr, moreArr));
+    sb.append(T.solution(team, damagedArr, spareArr));
     sb.append("\n");
 
     bw.write(sb.toString());
