597e94483ebf0a6032ee998073284eddde845440
jieun-dev1 <jkdev1324@gmail.com>
2022-12-30 17:57:50 +0900

    오답노트 1. ch[temp] = 0 때문에 시간초과가 났었음. 여기서 DFS 후 0을 Ch[temp] = 0을 하게 되면, 완전 탐색을 해야함. 반면, ch[temp]=1이 되었을때, 이를 내버려두면, 1인 것은 지나가게 됨. 최장거리 하나를 찾는 게 아니라, 지점 방문 여부를 찾는 것이므로 0을 풀어줄 필요 x.
    2.
    처음 시간 초과 시도 에서는
    앞의 완전 탐색 +
    answerCh 배열을 따로 만들어서, 여기에서는 제거를 하지 않음으로써 answer 개수를 셌다.

--- 2021JavaAlgorithm/src/BaekJoon/BFS/BJ2606.java
+++ 2021JavaAlgorithm/src/BaekJoon/BFS/BJ2606.java
@@ -10,23 +10,21 @@ import java.util.StringTokenizer;
 public class BJ2606 {
   static List<Integer>[] list;
   static int[] ch;
-  static int[] answerCh;
   static int answer = 0;
-  static boolean flag = false;
+
   public void DFS(int start){
     List<Integer> adjacent = list[start];
     //adjacent가 빈 배열인지 확인해주지 않으면 NullPointer가 남
     if(adjacent!=null){ //isEmpty가 아님. 연결이 안되어 있을 경우 list[start]에 원소가 없음. 2) while -> if 로 변경 (while 이라면 무한 반복이라 못빠져나옴, if는 한번만 체크함)
+      //o(100)
       for(int i=0;i<adjacent.size();i++){
         int temp = adjacent.get(i); //temp는 인접리스트를 차례로 읽어오는 값이다. 모르고 ch[adjacent.get(i)]를 했는데, 이러면 0 혹은 1이 나옴
         if(ch[temp]==0){
           ch[temp]=1;
-          answerCh[temp]=1;
           DFS(temp); //static이 아니기 때문에 객체 생성해서 T 로 접근하지 않아도 됨.
-          ch[temp]=0;
+//          ch[temp]=0;
         }
       }
-      flag = true;
     }
 
   }
@@ -40,14 +38,15 @@ public class BJ2606 {
     int computer = Integer.parseInt(st.nextToken());
     list = new ArrayList[computer+1];
     ch = new int[computer+1];
-    answerCh= new int[computer+1];
     //연결 리스트 초기화 pair 개수 +1 만큼 만들어서, 인덱스신경 안쓰도록.
     st = new StringTokenizer(br.readLine());
     int pair = Integer.parseInt(st.nextToken());
+    //O(100)
     for(int i=1;i<computer+1;i++) { //pair+1로 만드는 실수.
     list[i] = new ArrayList<>();
     }
       //원소에 하나씩 ArrayList 넣어줘야. 양쪽에 만들어준다 양방향연결이니까.
+    //pair는 최대 10000개.
     for(int i=0;i<pair;i++) {
       st = new StringTokenizer(br.readLine());
       int x = Integer.parseInt(st.nextToken());
@@ -58,8 +57,9 @@ public class BJ2606 {
     T.DFS(1); //DFS 는 NON STATIC 메서드라서, NON-STATIC에서 REFER 불가.
 
     //answerCh가 아니라 ch를 체크함. ch는 DFS 끝나면 0으로 되서, DFS 문 모두 끝나면 다 0으로 초기화 됨.
-    for(int i=0;i<answerCh.length;i++){
-      if(answerCh[i]==1){
+    //o(100)
+    for(int i=0;i<ch.length;i++){
+      if(ch[i]==1){
         answer++;
       }
     }
