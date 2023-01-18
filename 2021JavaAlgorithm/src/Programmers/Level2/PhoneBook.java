230faa9016bc8aa380c2c37167cec93a3bb90c71
jieun-dev1 <jkdev1324@gmail.com>
2023-01-18 10:27:33 +0900

    해결
    (1) i번 인덱스에서 미리 i+1을 확인해서 비교한다. (이렇게 비교 횟수를 줄인다)
    
    즉, get(1).startsWith(get(0)) 부터 시작하는 것.
    하나라도 그런 경우가 있다면, answer 는 false  로 리턴.

--- 2021JavaAlgorithm/src/Programmers/Level2/PhoneBook.java
+++ 2021JavaAlgorithm/src/Programmers/Level2/PhoneBook.java
@@ -3,29 +3,38 @@ package Programmers.Level2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class PhoneBook {
   public boolean solution(String[] phone_book) {
+    if(phone_book.length==1){
+      return false;
+    }
     boolean answer = true;
-//    HashSet<String> set = new HashSet<>(Arrays.asList(phone_book));
+    HashMap<String, Integer> hm = new HashMap<>(); // 접두어로 시작하는 key를 가질 때, value에 추가해준다.
     List<String> list = new ArrayList<>(Arrays.asList(phone_book));
-    //O(
     Collections.sort(list);
-    for(int i=0;i<list.size()-1;i++){
-      String temp = list.get(i); //정렬되어 있으므로, 뒤의 원소가 더 길 것이다. 따라서 뒤의 원소와만 비교하면 된다.
-      for(int j=i+1;j<list.size();j++){
-        if(list.get(j).startsWith(temp)) {
-          answer = false;
-          return answer;
-        }
+    //put의 시간 복잡도 o(1) 최악 o(n)
+    for(int i=0;i< list.size()-1;i++){
+      String temp = list.get(i);
+      if(hm.isEmpty()){
+        hm.put(temp, 1);
+      }
+      //뒤의 것을 미리 확인하면, 순회하는 횟수가 준다.
+      if(list.get(i+1).startsWith(list.get(i))){
+        answer = false;
+        return answer;
       }
     }
+
+    //value가 1이 아닌 경우가 있다면, 접두어가 있는것.
     return answer;
   }
 
   public static void main(String[] args) {
     PhoneBook T = new PhoneBook();
     System.out.println(T.solution(new String[]{"119", "97674223", "1195524421"}));
+    System.out.println(T.solution(new String[]{"123", "3123"}));
   }
 }
